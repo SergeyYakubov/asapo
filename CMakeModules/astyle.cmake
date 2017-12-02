434c3ebd1a8720ef97ee4a4736b6bd3bda3b51a5
Sergey Yakubov <sergey.yakubov@desy.de>
2017-12-02 11:56:51 +0100

    remove extra ASTYLE target, correct exclude unittest template

--- CMakeModules/astyle.cmake
+++ CMakeModules/astyle.cmake
@@ -1,14 +1,12 @@
 find_program(ASTYLE_EXECUTABLE astyle)
 
-add_custom_target(ASTYLE)
-
 if(ASTYLE_EXECUTABLE)
 	message(STATUS "Found astyle, using astyle to format code of target ${target}.")
 	add_custom_target(astyle ALL
 		COMMAND
 		${ASTYLE_EXECUTABLE} -i
 			--exclude=${PROJECT_BINARY_DIR}
-			--recursive -n --style=google --indent=spaces=4
+			--recursive -n --style=google
 			"${PROJECT_SOURCE_DIR}/*.cpp" "${PROJECT_SOURCE_DIR}/*.h"
 		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		VERBATIM	
