94d686c9e363ae5f4ee1f1b0ad02f9445ba7f6b7
Sergey Yakubov <sergey.yakubov@desy.de>
2017-12-03 20:20:49 +0100

    use ASSERT_THAT instead of ASSERT_EQ & Co.

--- worker/api/cpp/unittests/test_worker_api.cpp
+++ worker/api/cpp/unittests/test_worker_api.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "worker/data_broker.h"
 #include "../src/folder_data_broker.h"
@@ -8,6 +8,10 @@ using hidra2::DataBroker;
 using hidra2::FolderDataBroker;
 using hidra2::WorkerErrorCode;
 
+using ::testing::Eq;
+using ::testing::Ne;
+using ::testing::Test;
+
 namespace {
 
 TEST(DataBrokerFactoryTests, CreateFolderDataSource) {
@@ -15,8 +19,8 @@ TEST(DataBrokerFactoryTests, CreateFolderDataSource) {
 
     auto data_broker = DataBrokerFactory::Create("path/to/file", &return_code);
 
-    EXPECT_EQ(return_code, WorkerErrorCode::ERR__NO_ERROR);
-    EXPECT_NE(dynamic_cast<FolderDataBroker*>(data_broker.release()), nullptr);
+    ASSERT_THAT(return_code, Eq(WorkerErrorCode::ERR__NO_ERROR));
+    ASSERT_THAT(dynamic_cast<FolderDataBroker*>(data_broker.release()), Ne(nullptr));
 }
 
 TEST(DataBrokerFactoryTests, FailCreateDataSourceWithEmptySource) {
@@ -24,11 +28,11 @@ TEST(DataBrokerFactoryTests, FailCreateDataSourceWithEmptySource) {
 
     auto data_broker = DataBrokerFactory::Create("", &return_code);
 
-    EXPECT_EQ(return_code, WorkerErrorCode::ERR__EMPTY_DATASOURCE);
-    EXPECT_EQ(data_broker.release(), nullptr);
+    ASSERT_THAT(return_code, Eq(WorkerErrorCode::ERR__EMPTY_DATASOURCE));
+    ASSERT_THAT(data_broker.release(), Eq(nullptr));
 }
 
-class FolderDataBrokerTests : public ::testing::Test {
+class FolderDataBrokerTests : public Test {
  public:
     FolderDataBroker* data_broker;
     void SetUp() override {
@@ -41,11 +45,9 @@ class FolderDataBrokerTests : public ::testing::Test {
     }
 };
 
-
-TEST_F(FolderDataBrokerTests, ConnectPreparesFileList){
-    auto return_code= data_broker->Connect();
-    EXPECT_EQ(return_code, WorkerErrorCode::ERR__NO_ERROR);
+TEST_F(FolderDataBrokerTests, ConnectPreparesFileList) {
+    auto return_code = data_broker->Connect();
+    ASSERT_THAT(return_code, Eq(WorkerErrorCode::ERR__NO_ERROR));
 }
-
 
 }
