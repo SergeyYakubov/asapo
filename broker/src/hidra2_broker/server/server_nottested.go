e16405403fc83ed0cf8339d6d6fb0cf5ea8edc53
Sergey Yakubov <sergey.yakubov@desy.de>
2018-03-01 10:52:31 +0100

    added manual test for broker performance

--- broker/src/hidra2_broker/server/server_nottested.go
+++ broker/src/hidra2_broker/server/server_nottested.go
@@ -3,11 +3,11 @@
 package server
 
 import (
+	"errors"
 	"hidra2_broker/utils"
 	"log"
 	"net/http"
 	"strconv"
-	"errors"
 )
 
 func StartStatistics() {
@@ -19,7 +19,7 @@ func StartStatistics() {
 func Start() {
 	StartStatistics()
 	mux := utils.NewRouter(listRoutes)
-	log.Fatal(http.ListenAndServe("localhost:"+strconv.Itoa(settings.Port), http.HandlerFunc(mux.ServeHTTP)))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(settings.Port), http.HandlerFunc(mux.ServeHTTP)))
 }
 
 func ReadConfig(fname string) error {
