c0234eed864cabac5fbffc326f4b408d46ad2fe5
Sergey Yakubov <sergey.yakubov@desy.de>
2018-08-27 10:39:13 +0200

    fix for windows

--- producer/event_monitor_producer/src/system_folder_watch_windows.cpp
+++ producer/event_monitor_producer/src/system_folder_watch_windows.cpp
@@ -1,5 +1,17 @@
-#include "system_folder_watch_linux.h"
+#include "system_folder_watch_windows.h"
 
 namespace asapo {
 
+Error SystemFolderWatch::StartFolderMonitor(const std::vector<std::string> &monitored_folders) {
+    return nullptr;
 }
+
+FileEvents SystemFolderWatch::GetFileEventList(Error* err) {
+    FileEvents events;
+    *err = nullptr;
+    return events;
+}
+
+
+}
+
--- producer/event_monitor_producer/src/system_folder_watch_windows.h
+++ producer/event_monitor_producer/src/system_folder_watch_windows.h
@@ -1,9 +1,20 @@
 #ifndef ASAPO_SYSTEM_FOLDER_WATCH_WINDOWS_H
 #define ASAPO_SYSTEM_FOLDER_WATCH_WINDOWS_H
 
+#include <vector>
+#include <string>
+
+#include "common/error.h"
+#include "preprocessor/definitions.h"
+#include "asapo_producer.h"
+#include "common.h"
+
+
 namespace asapo {
 
 class SystemFolderWatch {
+  Error StartFolderMonitor(const std::vector<std::string>& monitored_folders);
+  FileEvents GetFileEventList(Error* err);
 
 };
 
