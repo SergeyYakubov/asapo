c2b2323cf18f721b8500ddaf70028897697136ad
Sergey Yakubov <sergey.yakubov@desy.de>
2019-03-19 15:58:21 +0100

    change version tags for python

--- CMakeModules/prepare_version.cmake
+++ CMakeModules/prepare_version.cmake
@@ -1,16 +1,3 @@
-execute_process(COMMAND git describe --tags --dirty OUTPUT_VARIABLE VERSION)
-string(STRIP ${VERSION} VERSION)
-
-
-execute_process(COMMAND git rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE BRANCH)
-string(STRIP ${BRANCH} BRANCH)
-
-if (${BRANCH} STREQUAL "develop")
-    SET (ASAPO_VERSION ${BRANCH}.${VERSION})
-else()
-    SET (ASAPO_VERSION ${BRANCH}.latest)
-endif()
-
 string(TIMESTAMP TIMESTAMP "%H:%M:%S %d.%m.%Y UTC" UTC)
 
 configure_file(${PROJECT_SOURCE_DIR}/common/cpp/include/common/version.h.in ${PROJECT_SOURCE_DIR}/common/cpp/include/common/version.h @ONLY)
--- CMakeModules/prepare_version_tag.cmake
+++ CMakeModules/prepare_version_tag.cmake
@@ -10,3 +10,26 @@ else()
     SET (ASAPO_VERSION ${BRANCH}.latest)
 endif()
 
+execute_process(COMMAND git describe --tags --abbrev=0 OUTPUT_VARIABLE VERSION_TAGS)
+string(STRIP ${VERSION_TAGS} VERSION_TAGS)
+
+execute_process(COMMAND git rev-parse --short HEAD OUTPUT_VARIABLE VERSION_COMMIT)
+string(STRIP ${VERSION_COMMIT} VERSION_COMMIT)
+
+function(cleanup varname)
+    string (REPLACE "-" "_" out ${${varname}})
+    SET( ${varname} ${out} PARENT_SCOPE)
+endfunction()
+
+cleanup(BRANCH)
+
+
+if (${BRANCH} STREQUAL "develop")
+    SET (ASAPO_VERSION_PYTHON ${VERSION_TAGS}.${BRANCH}.${VERSION_COMMIT})
+else()
+    SET (ASAPO_VERSION_PYTHON ${VERSION_TAGS}.${BRANCH}.latest)
+endif()
+
+
+message("Asapo Version: " ${ASAPO_VERSION})
+message("Asapo Version Python: " ${ASAPO_VERSION_PYTHON})
