16ff4907f0e559447174f90dac621706ff850796
Sergey Yakubov <sergey.yakubov@desy.de>
2019-08-14 12:31:39 +0200

    fix memory leak, add test

---
+++ tests/automatic/bug_fixes/worker_python_memleak/check_linux.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -e
+
+trap Cleanup EXIT
+
+endpoint=127.0.0.1:8400
+path=.
+beamtime_id=asapo_test
+token="IEfwsWa0GXky2S3MkxJSUHJT1sI8DD5teRdjBUXVRxk="
+
+
+Cleanup() {
+    echo cleanup
+    rm -rf $fname
+    nomad stop nginx
+    nomad stop broker
+    nomad stop discovery
+    echo "db.dropDatabase()" | mongo ${beamtime_id}
+}
+
+fname=test.dat
+size=100000000 # 10MB
+
+nomad run nginx.nmd
+nomad run discovery.nmd
+nomad run broker.nmd
+
+sleep 1
+
+echo 'db.data.insert({"_id":'1',"size":'$size',"name":"'$fname'","lastchange":1,"source":"none","buf_id":0,"meta":{}})' | mongo ${beamtime_id}
+dd if=/dev/zero of=$fname bs=$size count=1
+
+export PYTHONPATH=$1:${PYTHONPATH}
+
+export PYTHONPATH=/home/yakubov/projects/asapo/cmake-build-debug/worker/api/python:${PYTHONPATH}
+
+
+python memleak.py $endpoint $path $beamtime_id $token &> out &
+pid=`echo $!`
+
+sleep 1
+
+mem1=`pmap -x $pid  | tail -n 1 | awk  '{print $3}'`
+
+sleep 3
+
+mem2=`pmap -x $pid  | tail -n 1 | awk  '{print $3}'`
+
+kill -9 $pid
+
+leak=$(( $mem2 - $mem1 ))
+
+cat out
+echo leak: $leak
+
+test $leak -eq 0
