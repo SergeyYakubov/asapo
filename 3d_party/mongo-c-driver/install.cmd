2d9f614254c6a001e2614a31c95308ee4a8cee4e
Sergey Yakubov <sergey.yakubov@desy.de>
2019-11-22 12:40:00 +0100

    cleanup install script

--- 3d_party/mongo-c-driver/install.cmd
+++ 3d_party/mongo-c-driver/install.cmd
@@ -7,14 +7,6 @@ SET dir=c:\tmp\mongo-c-driver-1.15.2
 set mypath=%cd%
 cd /d %dir%
 
-:: install libbson
-cd src\libbson
-cmake "-DCMAKE_INSTALL_PREFIX=C:\mongo-c-driver" ^
-      "-DCMAKE_BUILD_TYPE=Release" ^
-      "-DCMAKE_C_FLAGS_RELEASE=/MT"
-cmake --build . --config Release
-cmake --build . --target install --config Release
-
 :: install mongoc
 cd %dir%
 cmake "-DCMAKE_INSTALL_PREFIX=C:\mongo-c-driver" ^
