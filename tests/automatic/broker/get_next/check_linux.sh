43a99b8079d3a931abddf91368e580b985af2df7
Sergey Yakubov <sergey.yakubov@desy.de>
2019-12-19 09:45:01 +0100

    fix test

--- tests/automatic/broker/get_next/check_linux.sh
+++ tests/automatic/broker/get_next/check_linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 database_name=data_stream
+substream=substream
 
 set -e
 
@@ -12,8 +13,8 @@ Cleanup() {
 	kill -9 $brokerid
 }
 
-echo "db.data_default.insert({"_id":2})" | mongo ${database_name}
-echo "db.data_default.insert({"_id":1})" | mongo ${database_name}
+echo "db.data_${substream}.insert({"_id":2})" | mongo ${database_name}
+echo "db.data_${substream}.insert({"_id":1})" | mongo ${database_name}
 
 token=`$2 token -secret auth_secret.key data`
 
@@ -23,10 +24,10 @@ sleep 0.3
 brokerid=`echo $!`
 
 groupid=`curl -d '' --silent 127.0.0.1:5005/creategroup`
-curl -v  --silent 127.0.0.1:5005/database/data/stream/substream/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"_id":1'
-curl -v  --silent 127.0.0.1:5005/database/data/stream/substream/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"_id":2'
-curl -v  --silent 127.0.0.1:5005/database/data/stream/substream/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"id_max":2'
+curl -v  --silent 127.0.0.1:5005/database/data/stream/${substream}/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"_id":1'
+curl -v  --silent 127.0.0.1:5005/database/data/stream/${substream}/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"_id":2'
+curl -v  --silent 127.0.0.1:5005/database/data/stream/${substream}/${groupid}/next?token=$token --stderr - | tee /dev/stderr  | grep '"id_max":2'
 
 # with a new group
 groupid=`curl -d '' --silent 127.0.0.1:5005/creategroup`
-curl -v  --silent 127.0.0.1:5005/database/data/stream/substream/${groupid}/next?token=$token --stderr - | tee /dev/stderr | grep '"_id":1'
+curl -v  --silent 127.0.0.1:5005/database/data/stream/${substream}/${groupid}/next?token=$token --stderr - | tee /dev/stderr | grep '"_id":1'
