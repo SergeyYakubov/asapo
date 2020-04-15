9e6c89ef9a984bff26544195b2dd0778ee00fdc9
Carsten Patzke <carsten.patzke@desy.de>
2020-04-15 13:13:22 +0200

    [RequestPool] Fixed memcheck

--- common/cpp/include/common/networking.h
+++ common/cpp/include/common/networking.h
@@ -47,18 +47,20 @@ const std::size_t kPosDataSetId = 1;
 const std::size_t kPosDataSetSize = 2;
 
 struct GenericRequestHeader {
-    GenericRequestHeader(Opcode i_op_code = kOpcodeUnknownOp, uint64_t i_data_id = 0,
-                         uint64_t i_data_size = 0, uint64_t i_meta_size = 0, const std::string& i_message = "",
-                         const std::string& i_substream = ""):
-        op_code{i_op_code}, data_id{i_data_id}, data_size{i_data_size}, meta_size{i_meta_size} {
-        memcpy(message, i_message.c_str(), kMaxMessageSize);
-        strncpy(substream, i_substream.c_str(), kMaxMessageSize);
-    }
     GenericRequestHeader(const GenericRequestHeader& header) {
         op_code = header.op_code, data_id = header.data_id, data_size = header.data_size, meta_size = header.meta_size,
         memcpy(custom_data, header.custom_data, kNCustomParams * sizeof(uint64_t)),
         memcpy(message, header.message, kMaxMessageSize);
         strncpy(substream, header.substream, kMaxMessageSize);
+    }
+
+    /* THIS CONSTRUCTOR SHOULD ONLY BE CALLED FOR TESTS! (because the message is only being strncpy'ed */
+    GenericRequestHeader(Opcode i_op_code = kOpcodeUnknownOp, uint64_t i_data_id = 0,
+                         uint64_t i_data_size = 0, uint64_t i_meta_size = 0, const std::string& i_message = "",
+                         const std::string& i_substream = ""):
+        op_code{i_op_code}, data_id{i_data_id}, data_size{i_data_size}, meta_size{i_meta_size} {
+        strncpy(message, i_message.c_str(), kMaxMessageSize);
+        strncpy(substream, i_substream.c_str(), kMaxMessageSize);
     }
 
     Opcode      op_code;
