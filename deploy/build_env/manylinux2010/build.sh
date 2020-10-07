e0e48ae52c42fd64b59baf0f1e9ee9c378f69010
Tim Schoof <tim.schoof@desy.de>
2020-10-07 18:02:56 +0200

    Use specific numpy version per python version
    The numpy version is chosen based on the numpy version provided by
    debian and centos for python < 3.7 and the version used by scipy wheels
    for all other python version.
    
    Additionally, the producer
    and the consumer now have the same setup and install dependencies on
    cython.

--- deploy/build_env/manylinux2010/build.sh
+++ deploy/build_env/manylinux2010/build.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
+declare -A numpy_versions
+numpy_versions[cp27mu]=1.12.1
+numpy_versions[cp27m]=1.12.1
+numpy_versions[cp35m]=1.12.1
+numpy_versions[cp36m]=1.12.1
+numpy_versions[cp37m]=1.14.5
+numpy_versions[cp38]=1.17.3
+
 for python_path in /opt/python/cp{27,35,36,37}*m; do
+    python_version=$(basename $python_path)
+    python_version=${python_version#*-}
     python=$python_path/bin/python
     pip=$python_path/bin/pip
+    numpy_version=${numpy_versions[$python_version]}
+    echo "building wheel for python_version=$python_version with numpy_version=$numpy_version"
 
     cd /asapo/build
-    cmake -DENABLE_LIBFABRIC=off -DCMAKE_BUILD_TYPE="Release" -DLIBCURL_DIR=/curl -DPython_EXECUTABLE=$python ..
+    cmake -DENABLE_LIBFABRIC=off -DCMAKE_BUILD_TYPE="Release" -DLIBCURL_DIR=/curl -DPython_EXECUTABLE=$python -DNUMPY_VERSION=$numpy_version ..
     cd consumer \
-        && $pip install -r /asapo/consumer/api/python/dev-requirements.txt \
+        && $pip install -r api/python/dev-requirements.txt\
         && make \
         && $pip wheel api/python/source_dist_linux/dist/*.tar.gz -w wheelhouse --no-deps
     cd ../producer \
-        && $pip install -r /asapo/producer/api/python/dev-requirements.txt \
+        && $pip install -r api/python/dev-requirements.txt \
         && make \
         && $pip wheel api/python/source_dist_linux/dist/*.tar.gz -w wheelhouse --no-deps
 done
