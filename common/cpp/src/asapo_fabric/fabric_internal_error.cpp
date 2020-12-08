d654080cec5f84fcf89a533c7ab7ba0d469ef7cf
Sergey Yakubov <sergey.yakubov@desy.de>
2020-12-08 18:45:10 +0100

    create rpm package

--- common/cpp/src/asapo_fabric/fabric_internal_error.cpp
+++ common/cpp/src/asapo_fabric/fabric_internal_error.cpp
@@ -1,7 +1,7 @@
 #include "fabric_internal_error.h"
 #include "fabric_function_map.h"
 #include <rdma/fi_errno.h>
-#include <asapo_fabric/fabric_error.h>
+#include "asapo/asapo_fabric/fabric_error.h"
 
 asapo::Error asapo::fabric::ErrorFromFabricInternal(const std::string& where, int internalStatusCode) {
     std::string errText = where + ": " + gffm().fi_strerror(-internalStatusCode);
--- deploy/build_env/centos/build.sh
+++ deploy/build_env/centos/build.sh
@@ -3,9 +3,14 @@
 cd /asapo/build
 cmake \
     -DCMAKE_BUILD_TYPE="Release" \
+    -DENABLE_LIBFABRIC=ON \
+    -DINSTALL_EXAMPLES=ON \
+    -DBUILD_CORE=OFF \
+    -DPACKAGE_RELEASE_SUFFIX=$OS \
     -DLIBCURL_DIR=/curl \
     ..
-cd consumer && make && make python-rpm && make python3-rpm
-cd ../producer && make && make python-rpm && make python3-rpm
+make package
+cd consumer/api/python/source_dist_linux && make python-rpm && make python3-rpm && cd -
+cd producer/api/python/source_dist_linux && make python-rpm && make python3-rpm
 
 
