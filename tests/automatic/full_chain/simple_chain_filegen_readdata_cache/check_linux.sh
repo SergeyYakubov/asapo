265ce32ee6eed7938580f0f9e7cb3f8d360e3fa5
Sergey Yakubov <sergey.yakubov@desy.de>
2021-05-20 18:06:55 +0200

    fix test

--- tests/automatic/full_chain/simple_chain_filegen_readdata_cache/check_linux.sh
+++ tests/automatic/full_chain/simple_chain_filegen_readdata_cache/check_linux.sh
@@ -71,7 +71,3 @@ grep "hello1" out.txt
 grep "hello2" out.txt
 grep "hello3" out.txt
 grep -i "Using connection type: $network_type" out.txt
-
-sleep 12
-
-influx -execute "select sum(n_requests) from statistics where receiver_ds_tag !=''" -database=${monitor_database_name} -format=json  | jq .results[0].series[0].values[0][1] | tee /dev/stderr | grep 3
