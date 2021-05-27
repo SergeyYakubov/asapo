dfcb5249e84aad6242a06562c79aa121f1d5beb6
Sergey Yakubov <sergey.yakubov@desy.de>
2021-05-28 01:24:14 +0200

    improve service start

--- tests/automatic/common_scripts/start_services.sh
+++ tests/automatic/common_scripts/start_services.sh
@@ -1,16 +1,27 @@
-nomad run receiver_tcp.nmd
 nomad run authorizer.nmd
 nomad run discovery.nmd
-nomad run broker.nmd
 nomad run nginx.nmd
+
+while true
+do
+  sleep 1
+  echo starting nginx and discovery
+  curl --silent 127.0.0.1:8400/asapo-discovery/asapo-mongodb --stderr - | grep 127.0.0.1  || continue
+  echo nginx and discovery started
+  break
+done
+
+
 nomad run file_transfer.nmd
+nomad run receiver_tcp.nmd
+nomad run broker.nmd
 
 while true
 do
   sleep 1
   echo starting services
   curl --silent 127.0.0.1:8400/asapo-discovery/v0.1/asapo-receiver?protocol=v0.1 --stderr - | grep 127.0.0.1  || continue
-  echo nginx,discovery, recevier started
+  echo recevier started
   curl --silent 127.0.0.1:8400/asapo-discovery/v0.1/asapo-broker?protocol=v0.1 --stderr - | grep 127.0.0.1 || continue
   echo broker started
   curl --silent 127.0.0.1:8400/asapo-discovery/v0.1/asapo-file-transfer?protocol=v0.1 --stderr -  | grep 127.0.0.1 || continue
