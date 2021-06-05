3d90a96250c7886df8f235a086013dc1376a3bb8
Sergey Yakubov <sergey.yakubov@desy.de>
2021-06-05 15:34:56 +0200

    fix win

--- tests/automatic/mongo_db/insert_retrieve/cleanup_windows.bat
+++ tests/automatic/mongo_db/insert_retrieve/cleanup_windows.bat
@@ -1,4 +1,4 @@
-SET database_name=data_%2F%20%5C%2E%22%24
+SET database_name=data_%%2F%%20%%5C%%2E%%22%%24
 SET mongo_exe="c:\Program Files\MongoDB\Server\4.2\bin\mongo.exe"
 
 echo db.dropDatabase() | %mongo_exe% %database_name%
