cefbdff2675f05557602bf3cbcbb451dddba9a19
Sergey Yakubov <sergey.yakubov@desy.de>
2021-07-07 11:06:26 +0200

    fix cmake config

--- CMakeIncludes/dependencies.cmake
+++ CMakeIncludes/dependencies.cmake
@@ -7,6 +7,18 @@ set (CMAKE_PREFIX_PATH "${LIBCURL_DIR}")
 find_package (CURL REQUIRED)
 message (STATUS "Found cURL libraries: ${CURL_LIBRARIES}")
 message (STATUS "cURL include: ${CURL_INCLUDE_DIRS}")
+if(CURL_FOUND) #old FindCURL versions do not create CURL::libcurl target, so we do it here if CURL::libcurl is missing
+    if(NOT TARGET CURL::libcurl)
+        add_library(CURL::libcurl UNKNOWN IMPORTED)
+        set_target_properties(CURL::libcurl PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
+        set_target_properties(CURL::libcurl PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                IMPORTED_LOCATION "${CURL_LIBRARIES}")
+    endif()
+endif()
+
+
 
 # python is needed anyway, even if no Python packages are build (e.g. to parse test results)
 if ("${Python_EXECUTABLE}" STREQUAL "")
