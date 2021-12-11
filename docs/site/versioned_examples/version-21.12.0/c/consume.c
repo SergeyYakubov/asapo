37f224cd2443a07e861e08694b1712fc76e18d42
Sergey Yakubov <sergey.yakubov@desy.de>
2021-12-11 13:41:48 +0100

    freeze 21.12.0

---
+++ docs/site/versioned_examples/version-21.12.0/c/consume.c
@@ -0,0 +1,66 @@
+#include "asapo/consumer_c.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void exit_if_error(const char *error_string, const AsapoErrorHandle err) {
+    if (asapo_is_error(err)) {
+        char buf[1024];
+        asapo_error_explain(err, buf, sizeof(buf));
+        printf("%s %s\n", error_string, buf);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    AsapoErrorHandle err = asapo_new_handle();
+    AsapoMessageMetaHandle mm = asapo_new_handle();
+    AsapoMessageDataHandle data = asapo_new_handle();
+
+    /* create snippet_start */
+    const char *endpoint = "localhost:8400";
+    const char *beamtime = "asapo_test";
+    const char *token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk1NzE3MTAyMTYsImp0aSI6ImMzaXFhbGpmNDNhbGZwOHJua20wIiwic3ViIjoiYnRfYXNhcG9fdGVzdCIsIkV4dHJhQ2xhaW1zIjp7IkFjY2Vzc1R5cGVzIjpbIndyaXRlIiwicmVhZCJdfX0.dkWupPO-ysI4t-jtWiaElAzDyJF6T7hu_Wz_Au54mYU";
+
+    const char * path_to_files = "/var/tmp/asapo/global_shared/data/test_facility/gpfs/test/2019/data/asapo_test"; //set it according to your configuration.
+
+    AsapoSourceCredentialsHandle cred = asapo_create_source_credentials(kProcessed,
+                                                                        beamtime,
+                                                                        "", "test_source", token);
+    AsapoConsumerHandle consumer = asapo_create_consumer(endpoint,
+                                                         path_to_files, 1,
+                                                         cred,
+                                                         &err);
+    asapo_free_handle(&cred);
+    /* create snippet_end */
+
+    exit_if_error("Cannot create consumer", err);
+    asapo_consumer_set_timeout(consumer, 5000ull);
+
+    /* consume snippet_start */
+    AsapoStringHandle group_id = asapo_consumer_generate_new_group_id(consumer, &err);
+    exit_if_error("Cannot create group id", err);
+
+    asapo_consumer_get_next(consumer, group_id, &mm, &data, "default",&err);
+    exit_if_error("Cannot get next record", err);
+
+    printf("id: %llu\n", (unsigned long long)asapo_message_meta_get_id(mm));
+    printf("file name: %s\n", asapo_message_meta_get_name(mm));
+    printf("file content: %s\n", asapo_message_data_get_as_chars(data));
+    /* consume snippet_end */
+
+    /* delete snippet_start */
+    asapo_consumer_delete_stream(consumer,"default", 1,1,&err);
+    exit_if_error("Cannot delete stream", err);
+    printf("stream deleted\n");
+    /* delete snippet_end */
+
+    asapo_free_handle(&err);
+    asapo_free_handle(&mm);
+    asapo_free_handle(&data);
+    asapo_free_handle(&consumer);
+    asapo_free_handle(&group_id);
+    return EXIT_SUCCESS;
+}
+
---
+++ docs/site/versioned_examples/version-21.12.0/cpp/consume_dataset.cpp
@@ -0,0 +1,71 @@
+#include "asapo/asapo_consumer.h"
+#include <iostream>
+
+
+void exit_if_error(std::string error_string, const asapo::Error& err) {
+    if (err) {
+        std::cerr << error_string << std::endl << err << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    asapo::Error err;
+
+    auto endpoint = "localhost:8400";
+    auto beamtime = "asapo_test";
+
+    auto token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJl"
+                 "eHAiOjk1NzE3MTAyMTYsImp0aSI6ImMzaXFhbGpmN"
+                 "DNhbGZwOHJua20wIiwic3ViIjoiYnRfYXNhcG9fdG"
+                 "VzdCIsIkV4dHJhQ2xhaW1zIjp7IkFjY2Vzc1R5cGV"
+                 "zIjpbIndyaXRlIiwicmVhZCJdfX0.dkWupPO-ysI4"
+                 "t-jtWiaElAzDyJF6T7hu_Wz_Au54mYU";
+
+    auto path_to_files = "/var/tmp/asapo/global_shared/data/test_facility/gpfs/test/2019/data/asapo_test";
+
+    auto credentials = asapo::SourceCredentials{asapo::SourceType::kProcessed, beamtime, "", "test_source", token};
+
+    auto consumer = asapo::ConsumerFactory::CreateConsumer(endpoint, path_to_files, true, credentials, &err);
+    exit_if_error("Cannot create consumer", err);
+    consumer->SetTimeout((uint64_t) 5000);
+
+    auto group_id = consumer->GenerateNewGroupId(&err);
+    exit_if_error("Cannot create group id", err);
+
+    // dataset snippet_start
+    asapo::DataSet ds;
+    asapo::MessageData data;
+
+    do {
+        ds = consumer->GetNextDataset(group_id, 0, "default", &err);
+
+        if (err && err == asapo::ConsumerErrorTemplates::kStreamFinished) {
+            std::cout << "stream finished" << std::endl;
+            break;
+        }
+
+        if (err && err == asapo::ConsumerErrorTemplates::kEndOfStream) {
+            std::cout << "stream ended" << std::endl;
+            break;
+        }
+        exit_if_error("Cannot get next record", err); // snippet_end_remove
+
+        std::cout << "Dataset Id: " << ds.id << std::endl;
+
+        for(int i = 0; i < ds.content.size(); i++)
+        {
+            err = consumer->RetrieveData(&ds.content[i], &data);
+            exit_if_error("Cannot get dataset content", err); // snippet_end_remove
+
+            std::cout << "Part " << ds.content[i].dataset_substream << " out of " << ds.expected_size << std:: endl;
+            std::cout << "message content: " << reinterpret_cast<char const*>(data.get()) << std::endl;
+        }
+    } while (1);
+    // dataset snippet_end
+
+    err = consumer->DeleteStream("default", asapo::DeleteStreamOptions{true, true});
+    exit_if_error("Cannot delete stream", err);
+
+    return EXIT_SUCCESS;
+}
