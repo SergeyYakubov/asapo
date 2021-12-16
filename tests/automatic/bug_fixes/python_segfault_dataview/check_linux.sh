c2306483d00d86e8cc9b85a0d013fd7971dc53ad
Sergey Yakubov <sergey.yakubov@desy.de>
2021-12-16 13:48:16 +0100

    fix segfault

--- tests/automatic/bug_fixes/python_segfault_dataview/check_linux.sh
+++ tests/automatic/bug_fixes/python_segfault_dataview/check_linux.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
+trap Cleanup EXIT
+
+Cleanup() {
+    echo cleanup
+    rm -rf ${receiver_root_folder}
+    echo "db.dropDatabase()" | mongo ${beamtime_id}_detector
+}
+
+
+
 export PYTHONPATH=$2:${PYTHONPATH}
 export Python3_EXECUTABLE=$3
 
-$Python3_EXECUTABLE $1 $endpoint $beamtime_id $token
+beamline=test
+receiver_root_folder=/tmp/asapo/receiver/files
+facility=test_facility
+year=2019
+beamtime_id=asapo_test
+receiver_folder=${receiver_root_folder}/${facility}/gpfs/${beamline}/${year}/data/${beamtime_id}
+endpoint=127.0.0.1:8400
+
+mkdir -p ${receiver_folder}
+echo ${receiver_folder}
+$Python3_EXECUTABLE $1 $endpoint $beamtime_id
 
 
--- tests/automatic/bug_fixes/python_segfault_dataview/producer_segfault.py
+++ tests/automatic/bug_fixes/python_segfault_dataview/producer_segfault.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 import asapo_producer
 import time
 import numpy as np
+import sys
+
+endpoint, beamtime = sys.argv[1:]
 
 def callback(payload, err):
     if isinstance(err, asapo_producer.AsapoServerWarning):
@@ -12,8 +15,8 @@ def callback(payload, err):
     else:
         print("successfuly sent: ", payload)
 
-producer = asapo_producer.create_producer("google.com:8700",'processed', "beamtime", 'auto', "data_source", "token", 4, 5000)
+producer = asapo_producer.create_producer(endpoint,'processed', beamtime, 'auto', "data_source", '', 4, 5000)
 data = np.random.random((100, 100))
 
 producer.send(1, exposed_path="processed/foo.dat", stream="foo", data=data.view(np.int8), callback=callback)
-
+producer.wait_requests_finished(5000)
