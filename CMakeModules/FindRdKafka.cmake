3de6ad45c8b6fe9df145940a36b101ea324bed2f
Sergey Yakubov <sergey.yakubov@desy.de>
2022-01-24 11:36:37 +0100

    relax cmake reqs for kafka

--- CMakeModules/FindRdKafka.cmake
+++ CMakeModules/FindRdKafka.cmake
@@ -7,7 +7,7 @@
 # RDKAFKA_LIBRARIES         - Path to the library
 # RDKAFKA_INCLUDE_DIR     - Path to the include dir
 
-cmake_minimum_required(VERSION 3.12)
+cmake_minimum_required(VERSION 3.7)
 
 find_path(RDKAFKA_INCLUDE_DIR librdkafka/rdkafka.h HINTS ${RdKafka_DIR}/include)
 find_library(RDKAFKA_LIBRARIES rdkafka++ HINTS ${RdKafka_DIR}/lib ${RdKafka_DIR}/lib64)
